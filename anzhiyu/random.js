37f30b92972983feb21b6ec701c72a18da2fe408
zandouzai <zandouzai123@outlook.com>
2025-05-01 23:04:02 +0800

    Site updated: 2025-05-01 23:04:01

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/29/网络如何连接/","2025/04/29/hello-world/"];function toRandomPost(){
+var posts=["2025/04/29/hello-world/","2025/04/29/网络如何连接/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
