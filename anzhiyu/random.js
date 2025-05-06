200612e0c53179d6adcf5a46690ea5aa97925b79
zandouzai <zandouzai123@outlook.com>
2025-05-06 19:25:33 +0800

    Site updated: 2025-05-06 19:25:32

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/29/hello-world/","2025/04/29/网络如何连接/"];function toRandomPost(){
+var posts=["2025/04/29/hello-world/","2025/05/06/搭建可访问的博客（搭建图床-搭建框架）的详细（啰嗦）步骤/","2025/04/29/网络如何连接/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
