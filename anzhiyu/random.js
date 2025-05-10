892a655ef82ce83300385e1738bc12d01383e705
zandouzai <zandouzai123@outlook.com>
2025-05-10 22:29:06 +0800

    Site updated: 2025-05-10 22:29:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/29/hello-world/","2025/05/08/搭建博客的教程（二）框架-主题/","2025/05/08/搭建博客的教程（一）图床/"];function toRandomPost(){
+var posts=["2025/04/29/hello-world/","2025/05/08/搭建博客的教程（一）图床/","2025/05/08/搭建博客的教程（二）框架-主题/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
